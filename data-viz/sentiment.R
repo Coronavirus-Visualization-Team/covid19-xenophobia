6edccfc92df1fcc81299754dd01aaf8dfd06ab15
Michelle Pan <michellepan8@gmail.com>
2020-12-14 23:34:13 -0800

    add labels to sentiment graphs

--- data-viz/sentiment.R
+++ data-viz/sentiment.R
@@ -1,64 +1,62 @@
 library(ggplot2)
+library(grid)
 
 setwd("/Users/michelle/Documents/covid19-xenophobia/data-viz")
 data <- read.csv("../sentiment-data/agg_filled.csv")
 
-sum(filter(data, date >= as.Date("2020-01-27"))$total)
 data$date <- as.Date(data$date)
 
-ggplot(data, aes(date, neg_count)) +
+spikes <- as.Date(c(
+  "2020-03-09",
+  "2020-03-18",
+  "2020-03-26",
+  "2020-05-01",
+  "2020-06-21",
+  "2020-07-10",
+  "2020-07-21",
+  "2020-09-11",
+  "2020-09-16",
+  "2020-10-02"))
+
+add_labels = function(plot, h) {
+  plot + geom_label(aes(x = as.Date(spikes[1]), y = h, label = "A"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[2]), y = h, label = "B"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[3]), y = h, label = "C"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[4]), y = h, label = "D"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[5]), y = h, label = "E"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[6]), y = h, label = "F"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[7]), y = h, label = "G"), label.size = 0) +
+  geom_label(aes(x = as.Date(spikes[8]), y = h, label = "H"), label.size = 0, label.padding = unit(0.1, "lines")) +
+  geom_label(aes(x = as.Date(spikes[9]), y = h, label = "I"), label.size = 0, label.padding = unit(0.1, "lines")) +
+  geom_label(aes(x = as.Date(spikes[10]), y = h, label = "J"), label.size = 0)
+}
+
+plot1 <- ggplot(data, aes(date, neg_count)) +
   geom_line(na.rm = TRUE) +
-  xlab("date") +
-  ylab("negative tweet count") +
-  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-01-27"), NA)) +
+  xlab("Date (2020)") +
+  ylab("Number of Negative Tweets") +
+  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
   scale_y_continuous(breaks = round(seq(min(data$neg_count), max(data$neg_count),
                                         by = 2500), 1)) + theme_bw() +
   theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank()) +
-  geom_vline(xintercept = as.Date(c(
-    "2020-01-28",
-    "2020-02-07",
-    "2020-03-09",
-    "2020-03-18",
-    "2020-03-26",
-    "2020-04-16",
-    "2020-05-01",
-    "2020-06-21",
-    "2020-07-10",
-    "2020-07-21",
-    "2020-09-16",
-    "2020-10-02")), linetype = 3, colour = "black")
-g1grob <- ggplotGrob(g1)
-print(g1grob)
+  geom_vline(xintercept = spikes, linetype = 3, colour = "black") + 
+  coord_cartesian(ylim = c(0, 14000), clip = "off") + 
+  theme(plot.margin = unit(c(3,1,1,1), "lines"))
 
-# scale_x_continuous(name = "date", 
-#                    limits = c(as.Date("2020-01-27"), NA), 
-#                    breaks = round(seq(min(data$date), max(data$date), 
-#                                       by = as.difftime(30, units="days")), 1))
-# theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+print(add_labels(plot1, 15200))
 
-ggplot(data, aes(date, neg_count / total)) +
+plot2 <- ggplot(data, aes(date, neg_count / total)) +
   geom_line(na.rm = TRUE) +
-  xlab("date") +
-  ylab("negative tweet proportion") +
-  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-01-27"), NA)) +
+  xlab("Date (2020)") +
+  ylab("Proportion of Negative Tweets") +
+  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
   scale_y_continuous(breaks = round(seq(min(data$neg_count / data$total), max(data$neg_count / data$total),
                                         by = 0.1), 1)) + theme_bw() +
   theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank()) +
-  geom_vline(xintercept = as.Date(c(
-      "2020-01-28",
-      "2020-02-07",
-      "2020-03-09",
-      "2020-03-18",
-      "2020-03-26",
-      "2020-04-16",
-      "2020-05-01",
-      "2020-06-21",
-      "2020-07-10",
-      "2020-07-21",
-      "2020-09-16",
-      "2020-10-02")),linetype = 3, colour = "black")
+  coord_cartesian(ylim = c(0, 0.525), clip = "off") + 
+  theme(plot.margin = unit(c(3,1,1,1), "lines")) +
+  geom_vline(xintercept = spikes,linetype = 3, colour = "black")
 
-
-              
+print(add_labels(plot2, 0.575))
