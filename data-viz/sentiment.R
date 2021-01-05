426c00f4a377552d374fa47864e94d10c34d6a57
Michelle Pan <michellepan8@gmail.com>
2021-01-05 13:44:26 -0800

    remove november label and add plot with negative/netural/positive values

--- data-viz/sentiment.R
+++ data-viz/sentiment.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(grid)
+library(reshape2)
 
 setwd("/Users/michelle/Documents/covid19-xenophobia/data-viz")
 data <- read.csv("../sentiment-data/agg_filled.csv")
@@ -35,7 +36,7 @@ plot1 <- ggplot(data, aes(date, neg_count)) +
   geom_line(na.rm = TRUE) +
   xlab("Date (2020)") +
   ylab("Number of Negative Tweets") +
-  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
+  scale_x_date(breaks = seq.Date(as.Date("2020-02-01"), as.Date("2020-10-31"), by = "month"), minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
   scale_y_continuous(breaks = round(seq(min(data$neg_count), max(data$neg_count),
                                         by = 2500), 1)) + theme_bw() +
   theme(panel.grid.major.x = element_blank(),
@@ -46,11 +47,32 @@ plot1 <- ggplot(data, aes(date, neg_count)) +
 
 print(add_labels(plot1, 15200))
 
-plot2 <- ggplot(data, aes(date, neg_count / total)) +
+data2 <- data
+data2$neutral_count <- data2$total - data2$neg_count
+dataMelted <- reshape2::melt(data2[ , c("date", "neg_count", "neutral_count")], id.var="date")
+
+plot2 <- ggplot(dataMelted, aes(x = date, y = value, col = variable)) + 
+  # geom_line(na.rm = TRUE, data = data, aes(date, neg_count), color = "red", key_glyph = draw_key_rect) +
+  # geom_line(na.rm = TRUE, data = data, aes(date, total - neg_count), color = "blue", key_glyph = draw_key_rect) +
+  geom_line(na.rm = TRUE) +
+  xlab("Date (2020)") +
+  ylab("Number of Tweets") +
+  scale_x_date(breaks = seq.Date(as.Date("2020-02-01"), as.Date("2020-10-31"), by = "month"), minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
+  scale_y_continuous(breaks = round(seq(0, max(data$total),
+                                        by = 10000), 1)) + theme_bw() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  coord_cartesian(ylim = c(0, 50000), clip = "off") + 
+  labs(colour = "key") + 
+  scale_color_manual(labels = c("negative", "postive/neutral"), values = c("#F8766D", "#06BEC4"))
+
+print(plot2)
+
+plot3 <- ggplot(data, aes(date, neg_count / total)) +
   geom_line(na.rm = TRUE) +
   xlab("Date (2020)") +
   ylab("Proportion of Negative Tweets") +
-  scale_x_date(date_breaks = "1 month", minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
+  scale_x_date(breaks = seq.Date(as.Date("2020-02-01"), as.Date("2020-10-31"), by = "month"), minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
   scale_y_continuous(breaks = round(seq(min(data$neg_count / data$total), max(data$neg_count / data$total),
                                         by = 0.1), 1)) + theme_bw() +
   theme(panel.grid.major.x = element_blank(),
@@ -59,4 +81,23 @@ plot2 <- ggplot(data, aes(date, neg_count / total)) +
   theme(plot.margin = unit(c(3,1,1,1), "lines")) +
   geom_vline(xintercept = spikes,linetype = 3, colour = "black")
 
-print(add_labels(plot2, 0.575))
+print(add_labels(plot3, 0.575))
+
+data3 <- data2
+data3$neg_prop <- data3$neg_count / data3$total
+data3$neutral_prop <- data3$neutral_count / data3$total
+dataMelted2 <- reshape2::melt(data3[ , c("date", "neg_prop", "neutral_prop")], id.var="date")
+
+plot4 <- ggplot(dataMelted2, aes(x = date, y = value, col = variable)) +
+  geom_line(na.rm = TRUE) +
+  xlab("Date (2020)") +
+  ylab("Proportion of Tweets") +
+  scale_x_date(breaks = seq.Date(as.Date("2020-02-01"), as.Date("2020-10-31"), by = "month"), minor_breaks = NULL, date_labels = "%b", limits = c(as.Date("2020-02-01"), NA)) +
+  scale_y_continuous(breaks = round(seq(0, 0.95,
+                                        by = 0.1), 1)) + theme_bw() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  coord_cartesian(ylim = c(0, 0.95), clip = "off") + 
+  theme(plot.margin = unit(c(3,1,1,1), "lines"))
+
+print(plot4)
