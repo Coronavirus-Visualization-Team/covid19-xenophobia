681ffe7993c16d521323e44bc727329ed5e3b41d
Michelle Pan <michellepan8@gmail.com>
2021-01-05 15:08:26 -0800

    add packed bubble plot of top slurs

--- data-viz/slurs.R
+++ data-viz/slurs.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(packcircles)
 
 setwd("/Users/michelle/Documents/covid19-xenophobia/data-viz")
 data <- read.csv("../slur-data/slur_counts.csv")
@@ -29,3 +30,16 @@ ggplot(top_data, aes(reorder(slur, count), count)) +
   scale_y_continuous(trans = "log10") + theme_bw() +
   coord_flip()
 
+top_data2 <- top_data[order(-top_data$count),]
+packing <- circleProgressiveLayout(top_data2$count)
+top_data_c <- cbind(top_data2, packing)
+dat.gg <- circleLayoutVertices(packing, npoints = 50)
+
+ggplot() +
+  geom_polygon(data = dat.gg, aes(x, y, group = id, fill = as.factor(id)), colour = "black", alpha = 0.6) +
+  # geom_text(data = top_data_c, aes(x, y, size = count, label = slur)) +
+  scale_size_continuous(range = c(1,4)) +
+  labs(fill = "key") +
+  scale_fill_hue(labels = top_data2$slur) +
+  theme_void() +
+  coord_equal()
